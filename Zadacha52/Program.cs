3f6ef0c3f130a4a7ed02f1959dedcab14b359fb4
Anna <eekolegov@mail.ru>
2022-10-08 23:44:13 +0300

    Zadacha52

---
+++ Zadacha52/Program.cs
@@ -0,0 +1,58 @@
+﻿void Zadacha52()
+{
+    Random random= new Random();
+    int rows=random.Next(1,4);
+    int columns=random.Next(1,3);
+    Console.WriteLine ($"Массив  размером - строчек {rows}, столбцов {columns}");
+    int[,]numbers=new int[rows, columns];
+    
+    FillArray(numbers);
+    PrintArray (numbers);
+    Average(numbers);
+}
+
+void FillArray (int[,] numb)
+{
+    Random random= new Random();
+    int rows=numb.GetLength(0);
+    int columns=numb.GetLength(1);
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<columns; j++)
+        {
+            numb[i,j]=random.Next(1,3);
+        }
+    }
+}
+
+void PrintArray (int[,] numb)
+{
+    int rows=numb.GetLength(0);
+    int columns=numb.GetLength(1);
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<columns; j++)
+        {
+           Console.Write(numb[i,j]+"\t");
+        }
+        Console.WriteLine(); 
+    }    
+}
+void Average(int[,] numb)
+{
+    int rows=numb.GetLength(0);
+    int columns=numb.GetLength(1);
+    Console.Write ("Среднее арифметическое каждого столбца:");
+       
+    for (int j=0; j<columns; j++)
+    {
+        int sum=0; 
+        for (int i=0; i<rows; i++)
+        {
+            sum=sum+numb[i,j];
+        }
+        Console.Write($"{Math.Round((double) sum/rows,1)}; "); 
+    }
+}
+Zadacha52();
+
