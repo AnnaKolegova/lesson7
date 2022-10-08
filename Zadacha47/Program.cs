fa1d9e90f9cf6f1b7ab22dfae6ce500904241468
Anna <eekolegov@mail.ru>
2022-10-08 21:39:36 +0300

    Zadacha47

---
+++ Zadacha47/Program.cs
@@ -0,0 +1,44 @@
+﻿void Zadacha47()
+{
+    Random random= new Random();
+    int rows=random.Next(4,10);
+    int columns=random.Next(4,10);
+    Console.WriteLine ($"Массив  размером - строчек {rows}, столбцов {columns}");
+    double[,]numbers=new double[rows, columns];
+    
+    FillArray(numbers);
+    
+    PrintArray (numbers);
+    
+    
+}
+
+void FillArray (double[,] numb)
+{
+    Random random= new Random();
+    int rows=numb.GetLength(0);
+    int columns=numb.GetLength(1);
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<columns; j++)
+        {
+            numb[i,j]=Convert.ToDouble(random.Next(-100,101)/10.0);
+        }
+    }
+
+}
+
+void PrintArray (double[,] numb)
+{
+    int rows=numb.GetLength(0);
+    int columns=numb.GetLength(1);
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<columns; j++)
+        {
+           Console.Write(numb[i,j]+"\t");
+        }
+        Console.WriteLine(); 
+    }    
+}
+Zadacha47();
